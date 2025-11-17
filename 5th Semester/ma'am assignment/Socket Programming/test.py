e9cd85a00fb523f3072941fb8f3ce74ab52bc90d
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-11-17 19:37:25 +0600

    slide

--- 5th Semester/ma'am assignment/Socket Programming/test.py
+++ 5th Semester/ma'am assignment/Socket Programming/test.py
@@ -1,20 +1,20 @@
-import socket
-
+import socket 
 host = '127.0.0.1'
 port = 7000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((host,port))
 server.listen(1)
 
-print(f'Server listening on {host}:{port}')
+print(f'Calculator Server listening on {host}:{port}')
+
 while True:
     conn,addr = server.accept()
     print(f'Connected by {addr}')
 
-    data = conn.recv(1024).decode()
-    num1, operator, num2 = data.split()
-    num1, num2 = int(num1), int(num2)
+    data=conn.recv(1024).decode()
+    num1,operator,num2 = data.split()
+    num1,num2 = int(num1),int(num2)
 
     if operator == '+':
         result = num1 + num2
@@ -22,7 +22,7 @@ while True:
         result = num1 - num2
     elif operator == '*':
         result = num1 * num2
-    elif operator == '/':
+    elif operator == '/':  
         result = num1 // num2 if num2 != 0 else 'Error: Division by zero'
     elif operator == '%':
         result = num1 % num2 if num2 != 0 else 'Error: Division by zero'

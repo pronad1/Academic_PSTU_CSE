b4ad96227b1f7399de22e5b61620266d0da5a46b
pronad1 <ug2102049@cse.pstu.ac.bd>
2026-04-01 15:08:11 +0600

    modify

--- 6th Semester/Level-3, Semester-II/CIT 324  Simulation and Modeling Sessional/ch-4/arithmetic_congruential_random_num_generator.py
+++ 6th Semester/Level-3, Semester-II/CIT 324  Simulation and Modeling Sessional/ch-4/arithmetic_congruential_random_num_generator.py
@@ -10,10 +10,13 @@ def arithmetic_congruential_generator(seed1, seed2, m, num_of_random_numbers):
     return random_nums
 
 
+
 seed1 = int(input("Enter the first seed value: "))
 seed2 = int(input("Enter the second seed value: "))
 m = int(input("Enter the value of 'm' (should largest prime num): "))
 num_of_random_numbers = int(input("Enter the number of random numbers to generate: "))
+
+
 
 random_numbers = arithmetic_congruential_generator(seed1, seed2, m, num_of_random_numbers)
 print("Generated random numbers:")
